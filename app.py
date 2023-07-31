7b2e325e18ed3921681c3420350b72ed526741e2
BhinoSTV <68230279+BhinoSTV@users.noreply.github.com>
2023-07-31 00:21:14 +0000

    Put some edits

--- app.py
+++ app.py
@@ -9,6 +9,9 @@ print(__name__)
 def BinoCent():
   return render_template('home.html')
 
+@app.route('/about_me')
+def about_me():
+    return render_template('AboutMe.html')
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
